f571763e98adbe99035013a97d097aeff8bfbbfc
Youhana Hana <meyouhana@gmail.com>
2017-09-19 12:51:40 +0100

    update layout

--- build/push.sh
+++ build/push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -e
-cf login -a http://api.vanilla.coke.cf-app.com/ -u admin -p OL4VQNTQ3z3wcTNrU1cd2GHiU_vLJWTs  -o YH -s dev --skip-ssl-validation &&
+cf login -a http://api.vanilla.coke.cf-app.com/ -u admin -p OL4VQNTQ3z3wcTNrU1cd2GHiU_vLJWTs  -o YH --skip-ssl-validation &&
 cf push
