cce149a71373cc604e3e202a0df77736e69d47fb
matthewcosgrove <matthew.cosgrove@emc.com>
2017-09-28 15:32:52 +0100

    Added staged ERT version and display logic to UI

--- client/src/components/Environment/Environment.js
+++ client/src/components/Environment/Environment.js
@@ -42,6 +42,7 @@ class Environment extends Component {
     let url = this.props.env.products.buildInfo.buildUrl;
     let buildNumber = this.props.env.products.buildInfo.buildGlobalIdentifier;
     let failedJobName = this.props.env.products.buildInfo.failedJobName;
+    let stagedVersion = this.props.env.products.stagedVersion;
 
     return (
       <Col xs="12" sm="6" lg="4">
@@ -55,6 +56,9 @@ class Environment extends Component {
         <Card className="text-white bg-primary">
           <CardBlock className="card-body pb-0">
             <CardRow row= {{label:"Current ERT Version", value: this.props.env.products.currentVersion}} key={this.props.env.products.currentVersion}></CardRow>
+
+            {stagedVersion ?
+            <CardRow row= {{label:"Staged ERT Version", value: this.props.env.products.stagedVersion}} key={this.props.env.products.stagedVersion}></CardRow> : <CardRow row= {{label:"Staged ERT Version", value: "N/A"}}></CardRow>}
 
             <CardRow row= {{label:"Forthcoming ERT Version(S3)", value: this.props.env.products.currentVersionInS3}} key={this.props.env.products.currentVersionInS3}></CardRow>
 
