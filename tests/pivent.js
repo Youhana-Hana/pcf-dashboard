a692440f64b32cd5bbcc00e0be1bde778a3513f6
Youhana Hana <Youhana.Hana@emc.com>
2017-10-25 09:01:54 +0100

    completed pivnet

--- tests/pivent.js
+++ tests/pivent.js
@@ -12,9 +12,8 @@ const usT3S3 = require('../routes/fixtures/us-t3/dashboard-info-s3.json');
 const singaporeProdS3 = require('../routes/fixtures/singapore-prod/dashboard-info-s3.json');
 
 describe('pivnet', function() {
-
   beforeEach(function() {
-    sinon.stub(api, 'get').resolves(usProdOpsMan);
+    sinon.stub(api, 'get').resolves('[]');
   });
 
   afterEach(function() {
@@ -24,42 +23,76 @@ describe('pivnet', function() {
   it('should call api', function(done) {
     pivnet.get(function(err, data) {
       expect(err).to.not.exist;
-      expect(api.get.callCount).to.equal(1);
+      expect(api.get.callCount).to.equal(2);
       done();
     });
   });
   
-  it('should call pivent ert reuntime endpoint', function(done) {
+  it('should call pivent ert runtime endpoint', function(done) {
     pivnet.get(function(err, data) {
       expect(err).to.not.exist;
       expect(api.get.args[0][0]).to.equal('https://network.pivotal.io/api/v2/products/elastic-runtime/releases');
       done();
     });
   });
+
+  it('get top 3 minor versions', function(done) {
+    api.get.resolves(require('./fixtures/ertReleases.json'));
+
+    pivnet.get(function(err, data) {
+      expect(err).to.not.exist;
+      expect(data.pivnet.ertVersions).to.deep.equal(expectedErtVersions);
+      done(err);
+    });
+  });
+  
+  it('get top 3 opsManager minor versions', function(done) {
+      api.get.onCall(1).resolves(require('./fixtures/opsManagerReleases.json'));
+      pivnet.get(function(err, data) {
+        expect(api.get.callCount).to.equal(2);
+        expect(data.pivnet.opsManagerVersions.length).to.equal(3);
+        expect(data.pivnet.opsManagerVersions).to.deep.equal(expectedOpsManagerVersions);
+        done(err);
+    });
+  });
 });
 
-const expectedDecoratedUsProd = {
-    "foundation": "PROD",
-    "region": "US",
-    "currentVersionERT": "1.10.13",
-    "stagedVersionERT": "",
-    "currentVersionERTInS3": "1.10.17"
-}
-const expectedDecoratedUsT3 = {
-    "foundation": "T3",
-    "region": "US",
-    "currentVersionERT": "1.10.13",
-    "stagedVersionERT": "",
-    "currentVersionERTInS3": "1.10.18"
-}
-const expectedDecoratedSingaporeProd = {
-    "foundation": "PROD",
-    "region": "SINGAPORE",
-    "currentVersionERT": "1.10.12",
-    "stagedVersionERT": "1.10.13",
-    "currentVersionERTInS3": "1.10.17"
-}
+const expectedErtVersions = [{
+      "id": "ert-1.12.*",
+      "latest": "1.12.4",
+      "releaseDate": "2017-10-12",
+      "supportEndDate": "2018-06-30",
+      "releaseNotesUrl": "https://docs.pivotal.io/pivotalcf/1.12/pcf-release-notes/runtime-rn.html#1.12.4"
+    }, {
+      "id": "ert-1.11.*",
+      "latest": "1.11.16",
+      "releaseDate": "2017-10-12",
+      "supportEndDate": "2018-03-31",
+      "releaseNotesUrl": "https://docs.pivotal.io/pivotalcf/1-11/pcf-release-notes/runtime-rn.html#1.11.16"
+    }, {
+      "id": "ert-1.10.*",
+      "latest": "1.10.30",
+      "releaseDate": "2017-10-12",
+      "supportEndDate": "2017-12-31",
+      "releaseNotesUrl": "https://docs.pivotal.io/pivotalcf/1-10/pcf-release-notes/runtime-rn.html#1.10.30"
+    }];
 
-const expectedAggregate = {
-    environments: [expectedDecoratedUsProd, expectedDecoratedUsT3, expectedDecoratedSingaporeProd]
-}
+const expectedOpsManagerVersions = [{
+      "id": "opsman-1.12.*",
+      "latest": "1.12.2",
+      "releaseDate": "2017-10-05",
+      "supportEndDate": "2018-06-15",
+    "releaseNotesUrl": "https://docs.pivotal.io/pivotalcf/pcf-release-notes/#1.12.2"
+    }, {
+      "id": "opsman-1.11.*",
+      "latest": "1.11.12",
+      "releaseDate": "2017-09-25",
+      "supportEndDate": "2018-03-31",
+      "releaseNotesUrl": "https://docs.pivotal.io/pivotalcf/pcf-release-notes/#1.11.12"
+    }, {
+      "id": "opsman-1.10.*",
+      "latest": "1.10.17",
+      "releaseDate": "2017-09-25",
+      "supportEndDate": "2017-12-31",
+      "releaseNotesUrl": "https://docs.pivotal.io/pivotalcf/pcf-release-notes/#1.10.17"
+    }];
