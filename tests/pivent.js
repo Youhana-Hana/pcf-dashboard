4e5425b2e21d361f7d18173e394acba8d82256fe
Youhana Hana <youhana.hana@emc.com>
2017-11-02 10:40:05 +0000

    fix opsmanager release notes link

--- tests/pivent.js
+++ tests/pivent.js
@@ -41,6 +41,7 @@ describe('pivnet', function() {
 
     pivnet.get(function(err, data) {
       expect(err).to.not.exist;
+        expect(data.pivnet.ertVersions.length).to.equal(3);
       expect(data.pivnet.ertVersions).to.deep.equal(expectedErtVersions);
       done(err);
     });
@@ -48,11 +49,36 @@ describe('pivnet', function() {
   
   it('get top 3 opsManager minor versions', function(done) {
       api.get.onCall(1).resolves(require('./fixtures/opsManagerReleases.json'));
+      
       pivnet.get(function(err, data) {
-        expect(api.get.callCount).to.equal(2);
-        expect(data.pivnet.opsManagerVersions.length).to.equal(3);
-        expect(data.pivnet.opsManagerVersions).to.deep.equal(expectedOpsManagerVersions);
-        done(err);
+          try {
+              expect(api.get.callCount).to.equal(2);
+              expect(data.pivnet.opsManagerVersions.length).to.equal(3);
+              expect(data.pivnet.opsManagerVersions).to.deep.equal(expectedOpsManagerVersions);
+              return done(err);
+          } catch(err) {
+              return done(err);
+          }
+    });
+  });
+  
+  it('get correct payload', function(done) {
+    api.get.onCall(0).resolves(require('./fixtures/ertReleases.json'));
+    api.get.onCall(1).resolves(require('./fixtures/opsManagerReleases.json'));
+    
+    pivnet.get(function(err, data) {
+      try {
+        const expectedPayload = {
+          pivnet: {
+            ertVersions: expectedErtVersions,
+            opsManagerVersions: expectedOpsManagerVersions
+          }};
+
+        expect(data).to.deep.equal(expectedPayload);
+        return done(err);
+      } catch(err) {
+        return done(err);
+      }
     });
   });
 });
@@ -82,17 +108,17 @@ const expectedOpsManagerVersions = [{
       "latest": "1.12.2",
       "releaseDate": "2017-10-05",
       "supportEndDate": "2018-06-15",
-    "releaseNotesUrl": "https://docs.pivotal.io/pivotalcf/1-12/pcf-release-notes/"
+    "releaseNotesUrl": "https://docs.pivotal.io/pivotalcf/1-12/pcf-release-notes/opsmanager-rn.html#1.12.2"
     }, {
       "id": "opsman-1.11.*",
       "latest": "1.11.12",
       "releaseDate": "2017-09-25",
       "supportEndDate": "2018-03-31",
-      "releaseNotesUrl": "https://docs.pivotal.io/pivotalcf/1-11/pcf-release-notes/"
+        "releaseNotesUrl": "https://docs.pivotal.io/pivotalcf/1-11/pcf-release-notes/opsmanager-rn.html#1.11.12"
     }, {
       "id": "opsman-1.10.*",
       "latest": "1.10.17",
       "releaseDate": "2017-09-25",
       "supportEndDate": "2017-12-31",
-      "releaseNotesUrl": "https://docs.pivotal.io/pivotalcf/1-10/pcf-release-notes/"
+        "releaseNotesUrl": "https://docs.pivotal.io/pivotalcf/1-10/pcf-release-notes/opsmanager-rn.html#1.10.17"
     }];
