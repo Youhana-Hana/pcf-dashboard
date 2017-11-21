80c6881a759a7457153e7591377e7e78326b0c07
matthewcosgrove <matthew.cosgrove@emc.com>
2017-11-21 23:01:52 +0100

    Mark the stubbed versions as fake

--- tests/environments.js
+++ tests/environments.js
@@ -57,7 +57,7 @@ describe('environments', function() {
 const expectedDecoratedDeProd = {
     "foundation": "PROD",
     "region": "DE1",
-    "currentVersionERT": "1.11.16",
+    "currentVersionERT": "1.11.16 (fake)",
     "stagedVersionERT": "",
     "currentVersionERTInS3": "1.10.17"
 }
@@ -71,7 +71,7 @@ const expectedDecoratedUsQAT3 = {
 const expectedDecoratedSingaporeProd = {
     "foundation": "PROD",
     "region": "SG1",
-    "currentVersionERT": "1.11.16",
+    "currentVersionERT": "1.11.16 (fake)",
     "stagedVersionERT": "",
     "currentVersionERTInS3": "1.10.17"
 }
